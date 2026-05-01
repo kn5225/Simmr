3338ab8dfbf61585fbfeb2bd91fea57fea202eb6
kn5225 <kanav.nagpal@hotmail.com>
2026-05-01 18:14:09 -0400

    added spoonacular route with dotenv and axios

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+const path = require('path');
+const absolutePath = path.join(__dirname, '.env');
+
+require('dotenv').config({ path: absolutePath });
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3001
 
-app.get('/api/hello', (req,res) => {
-    res.json({ message: "hello from the server" })
-})
+
+app.get('/api/recipes', async (req, res) => {
+    try{
+    const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch', 
+        {params: {apiKey: process.env.SPOONACULAR_API_KEY,query: req.query.query}})
+    res.json({message: response.data})
+    }
+    catch(error) {
+        console.log(error.message)
+        res.status(500).send('API call failed')
+    }
+
+    })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
-})
+    })
