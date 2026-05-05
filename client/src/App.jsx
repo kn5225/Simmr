6cda6f9e86d2a84c436b2ef58f4ef82bd46cad2c
kn5225 <kanav.nagpal@hotmail.com>
2026-05-05 19:37:38 -0400

    added ingredient cart basics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,21 @@ function App(){
   const [query, setQuery] = useState('');
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searched, setSearched] = useState(false)
+  const [searched, setSearched] = useState(false);
+  const [cart,setCart] = useState([]);
   function handleSearch(){
-    if (query.trim.length===0){return}
+    if (query.trim().length===0){return}
     setLoading(true)
     fetch(`/api/recipes?query=${query}`)
     .then(rec => rec.json())
     .then(rec => setRecipes(rec.message.results))
     .then(() => setLoading(false))
     .then(() => setSearched(true))
+  }
+  function handleAddToCart(recipeId){
+    fetch(`/api/recipe/${recipeId}`)
+    .then(rec => rec.json())
+    .then(rec => setCart([...cart,...rec.message]))
   }
   return(
   <div> 
@@ -21,10 +27,14 @@ function App(){
     {loading && <p>Loading...</p>}
     {recipes.map(e => (
         <div key = {e.id}>
-          <h3 >{e.title}</h3> 
+          <h3>{e.title}</h3> 
           <img src = {e.image} alt = {e.title}/>
+          <button onClick={() => handleAddToCart(e.id)}>Add to Cart</button>
         </div>
       ))}
+    {cart.map(ingredient => (
+          <p key={ingredient.id}>{ingredient.original}</p> 
+    ))}
     {searched && !loading && (recipes.length === 0) && <p>no items found</p>}
   </div>
       )}
